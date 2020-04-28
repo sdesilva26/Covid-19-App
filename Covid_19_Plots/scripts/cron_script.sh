34180663db4812dc46f6acc146bdfb6fd2b52b36
Shane de Silva (Imperial Laptop) <sdesilva26@gmail.com>
2020-04-28 19:02:43 +0100

    update to cron_scripts.sh

--- Covid_19_Plots/scripts/cron_script.sh
+++ Covid_19_Plots/scripts/cron_script.sh
@@ -3,6 +3,6 @@ cd ~/Documents/Repositories/Covid-19-App/Covid_19_Plots/
 git checkout master
 ~/anaconda3/envs/covid19/bin/python ./scripts/data_script.py "./data/COVID-19-total-announced-deaths.xlsx" > /tmp/data_script.log 2>&1
 git add . &&
-git commit -m "cron commit with updated data" &&
+git commit -a -m "cron commit with updated data" &&
 git push heroku &&
 git push origin
