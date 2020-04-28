1ee18c7e26f4ac8e1d63bacc83145b02709ab00c
Shane de Silva (Imperial Laptop) <sdesilva26@gmail.com>
2020-04-28 18:43:34 +0100

    change to cron.script to account for change of folder name

--- Covid_19_Plots/scripts/cron_script.sh
+++ Covid_19_Plots/scripts/cron_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/Documents/Repositories/Portfolio/Covid_19_Plots/
+cd ~/Documents/Repositories/Covid-19-App/Covid_19_Plots/
 git checkout master
 ~/anaconda3/envs/covid19/bin/python ./scripts/data_script.py "./data/COVID-19-total-announced-deaths.xlsx" > /tmp/data_script.log 2>&1
 git add . &&
